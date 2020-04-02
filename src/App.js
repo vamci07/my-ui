a607428295929fedc7f3c4e7b7f939baa62882b4
Vamshi Maddur <vamci07@gmail.com>
2020-04-02 12:24:37 -0400

    Input Page
    Full width search bar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
 import React from "react";
 import { ThemeWrapper, myTheme } from "./theme";
-import {
-  useMediaQuery,
-  Container,
-  Box,
-  makeStyles
-} from "@material-ui/core";
-
-const useStyles = makeStyles(theme => ({
-  input: {
-    border: 'none',
-    background: "rgba(255,255,255, .2)",
-    padding: theme.spacing(3, 2),
-    boxShadow: "none",
-    borderRadius: 0,
-    fontSize: theme.typography.pxToRem(24),
-    color: theme.palette.common.white,
-    outline: 0,
-    width: "100%",
-    '&::placeholder': {
-      color: theme.palette.common.white,
-      opacity: 1
-    },
-    '&:hover, &:focus': {
-      borderBottom: `1px solid ${theme.palette.common.white}`
-    }
-  }
-}));
+import { useMediaQuery } from "@material-ui/core";
+import Info from "./pages/Info";
 
 function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -35,16 +10,7 @@ function App() {
     [prefersDarkMode]
   );
 
-  const styles = useStyles();
-  return (
-    <ThemeWrapper theme={theme}>
-      <Container>
-        <Box display="flex" alignItems="center" justifyContent="center" p={3}>
-          <input className={styles.input} type="text" placeholder="Enter Search Text"></input>
-        </Box>
-      </Container>
-    </ThemeWrapper>
-  );
+  return <ThemeWrapper theme={theme}><Info /></ThemeWrapper>;
 }
 
 export default App;
--- src/theme/ThemeWrapper.js
+++ src/theme/ThemeWrapper.js
@@ -10,9 +10,8 @@ import {
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
     html, body {
-        margin: 0;
-        padding: 0;
-        background-color: #3498db;
+      margin: 0;
+      padding: 0;
     }
 `;
 
